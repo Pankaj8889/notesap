a37f481dba9e939e599536ac3f24e284f495024b
Pankaj8889 <pankajsaini8889@gmail.com>
2025-01-07 20:07:52 +0530

    fething login details

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
-import reactLogo from "./assets/react.svg";
-import "./App.css";
+import { Amplify } from 'aws-amplify';
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import outputs from "../amplify_outputs.json";
+
+// Ensure Amplify is configured properly
+Amplify.configure(outputs);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={reactLogo} className="logo react" alt="React logo" />
-
-        <h1>Hello from Amplify</h1>
-      </header>
-    </div>
+    <Authenticator>
+      {({ signOut, user }) => {
+        // Log the user object to inspect its structure
+        console.log("User object:", user.signInDetails.loginId);
+        return (
+          <main>
+            <h1>Hello {user?.username}</h1>
+            <h1>Email: {user.signInDetails.loginId || "Not available"}</h1>
+            <button onClick={signOut}>Sign out</button>
+          </main>
+        );
+      }}
+    </Authenticator>
   );
 }
 
